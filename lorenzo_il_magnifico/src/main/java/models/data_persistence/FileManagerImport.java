db7e4f4076b010e1c71152c5314b25284a9649b5
paolo_cena <paolo.cena@mail.polimi.it>
2017-07-09 20:15:16 +0200

    Test class

--- lorenzo_il_magnifico/src/main/java/models/data_persistence/FileManagerImport.java
+++ lorenzo_il_magnifico/src/main/java/models/data_persistence/FileManagerImport.java
@@ -22,13 +22,18 @@ import models.cards.Deck;
 public class FileManagerImport implements Serializable{
 
     /**
+     * basic constructor
+     * */
+    public FileManagerImport(){}
+
+    /**
      * Method to import all developmentCard from file
      * */
-    public ArrayList<DevelopmentCard> acquireDevCard(){
+    public ArrayList<DevelopmentCard> acquireDevCard() {
         ArrayList<DevelopmentCard> developmentCards = new ArrayList<DevelopmentCard>();
         final Type dCardType = new TypeToken<ArrayList<DevelopmentCard>>(){}.getType();
         Gson gson = new Gson();
-        String dCardFile = "lorenzo_il_magnifico/src/main/resources/DevCard.json";
+        String dCardFile = "/Users/cp18393/IdeaProjects/Lorenzo-il-Magnifico/lorenzo_il_magnifico/src/main/resources/DevCard.json";
         try (Reader reader = new FileReader(dCardFile)) {
             developmentCards = gson.fromJson(reader, dCardType);
             for(int i = 0; i < developmentCards.size(); i++) {
@@ -51,7 +56,7 @@ public class FileManagerImport implements Serializable{
         ArrayList<LeaderCard> leaderCards = new ArrayList<LeaderCard>();
         final Type lCardType = new TypeToken<ArrayList<LeaderCard>>(){}.getType();
         Gson gson = new Gson();
-        String lCardFile = "lorenzo_il_magnifico/src/main/resources/LeaderCard.json";
+        String lCardFile = "/Users/cp18393/IdeaProjects/Lorenzo-il-Magnifico/lorenzo_il_magnifico/src/main/resources/LeaderCard.json";
         try (Reader reader = new FileReader(lCardFile)) {
             leaderCards = gson.fromJson(reader, lCardType);
             for(int i = 0; i < leaderCards.size(); i++) {
@@ -70,7 +75,7 @@ public class FileManagerImport implements Serializable{
         ArrayList<ExcommunicationTile> excommunicationTiles = new ArrayList<ExcommunicationTile>();
         final Type tilesType = new TypeToken<ArrayList<ExcommunicationTile>>(){}.getType();
         Gson gson = new Gson();
-        String tilesFiles = "lorenzo_il_magnifico/src/main/resources/ExcommunicationsTiles.json";
+        String tilesFiles = "/Users/cp18393/IdeaProjects/Lorenzo-il-Magnifico/lorenzo_il_magnifico/src/main/resources/ExcommunicationsTiles.json";
         try (Reader reader = new FileReader(tilesFiles)) {
             excommunicationTiles = gson.fromJson(reader, tilesType);
             for(int i = 0; i < excommunicationTiles.size(); i++) {
@@ -88,7 +93,7 @@ public class FileManagerImport implements Serializable{
     public Config acquireConfigurations(){
         Gson gson = new Gson();
         Config configFile = new Config();
-        String configFiles = "lorenzo_il_magnifico/src/main/resources/Config.json";
+        String configFiles = "/Users/cp18393/IdeaProjects/Lorenzo-il-Magnifico/lorenzo_il_magnifico/src/main/resources/Config.json";
         try (Reader reader = new FileReader(configFiles)) {
             configFile = gson.fromJson(reader, Config.class);
                 System.out.println(configFile);
@@ -107,7 +112,7 @@ public class FileManagerImport implements Serializable{
         final Type boardType = new TypeToken<ArrayList<PersonalBoard>>() {
         }.getType();
         Gson gson = new Gson();
-        String boardFiles = "lorenzo_il_magnifico/src/main/resources/PlayerBoard.json";
+        String boardFiles = "/Users/cp18393/IdeaProjects/Lorenzo-il-Magnifico/lorenzo_il_magnifico/src/main/resources/PlayerBoard.json";
         try (Reader reader = new FileReader(boardFiles)) {
             personalBoards = gson.fromJson(reader, boardType);
             for (int i = 0; i < personalBoards.size(); i++) {
---
+++ lorenzo_il_magnifico/src/test/java/models/data_persistence/FileManagerImportTest.java
@@ -0,0 +1,28 @@
+package models.data_persistence;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cp18393 on 09/07/17.
+ */
+public class FileManagerImportTest {
+    @Test
+    public void acquireDevCard() throws Exception {
+
+    }
+
+    @Test
+    public void acquireTiles() throws Exception {
+    }
+
+    @Test
+    public void acquireConfigurations() throws Exception {
+    }
+
+    @Test
+    public void acquirePersonalBoard() throws Exception {
+    }
+
+}
